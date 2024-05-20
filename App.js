6d4dda4e1354653aee6cbcce51c635af5a3c6d8f
Julia Leal <julialeal.t@gmail.com>
2024-05-20 08:48:15 -0300

    feat: initializing project

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import AppNavigation from './navigation/appNavigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppNavigation />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View, SafeAreaView } from "react-native";
+
+export default function HomeScreen() {
+    return (
+        <SafeAreaView>
+            <Text>HomeScreen</Text>
+        </SafeAreaView>
+    )
+}
